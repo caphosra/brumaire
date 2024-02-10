9a2b92eafa71459b120df9adc3a52ac45df18eb6
Akihisa Yagi <caphosra@gmail.com>
2024-02-10 15:36:13 +0900

    Have agents support multi-boards

--- brumaire/agent.py
+++ brumaire/agent.py
@@ -9,16 +9,16 @@ class AgentBase:
         """
         Declare the goal. It will returns a ndarray shaped (4,)
         """
-        return np.array([SUIT_SPADE, 12, SUIT_SPADE, 14 - 2])
+        return np.repeat(np.array([[SUIT_SPADE, 12, SUIT_SPADE, 14 - 2]]), board.board_num, axis=0)
 
     def discard(self, board: BoardData) -> np.ndarray:
-        decision = np.zeros(14)
-        decision[[0, 1, 2, 3]] = 1.
+        decision = np.zeros((board.board_num, 14))
+        decision[:, [0, 1, 2, 3]] = 1
         return decision
 
     def put_card(self, board: BoardData) -> np.ndarray:
-        decision = np.zeros(10)
-        decision[0] = 1.
+        decision = np.zeros((board.board_num, 10))
+        decision[:, 0] = 1
         return decision
 
     def tell_reward(self, reward: float):
@@ -31,13 +31,13 @@ class RandomAgent(AgentBase):
         super().__init__()
 
     def declare_goal(self, board: BoardData) -> ndarray:
-        return np.random.randint([0, 12, 0, 0], [4, 14, 4, 13])
+        return np.random.randint([0, 12, 0, 0], [4, 14, 4, 13], size=(board.board_num, 4))
 
     def discard(self, board: BoardData) -> np.ndarray:
-        decision = np.zeros(14)
+        decision = np.zeros((board.board_num, 14))
         choice = np.random.choice(np.arange(14), 4, replace=False)
-        decision[choice] = 1.
+        decision[:, choice] = 1.
         return decision
 
     def put_card(self, board: BoardData) -> np.ndarray:
-        return np.eye(10)[np.random.randint(0, 10)]
+        return np.repeat((np.eye(10)[np.random.randint(0, 10)])[None, :], board.board_num, axis=0)
