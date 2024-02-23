4bff4ed9c0bf6508061c3b836ca7c74dc48bcf72
Akihisa Yagi <caphosra@gmail.com>
2024-02-23 19:12:50 +0900

    Add method to write results of the evaluation

--- brumaire/record.py
+++ brumaire/record.py
@@ -1,9 +1,16 @@
 from __future__ import annotations
 import numpy as np
 from typing import Tuple
+from torch.utils.tensorboard import SummaryWriter
 
 from brumaire.board import BOARD_VEC_SIZE, board_from_vector
-from brumaire.constants import NDFloatArray, NDIntArray
+from brumaire.constants import (
+    NDFloatArray,
+    NDIntArray,
+    ROLE_ADJUTANT,
+    ROLE_ALLY,
+    ROLE_NAPOLEON,
+)
 
 TURN = 10
 
@@ -112,3 +119,22 @@ class Recorder:
             return default
         else:
             return win_count / board_count
+
+    def write_eval_result(
+        self, player: int, writer: SummaryWriter, step: int = 0
+    ) -> None:
+        reward = self.avg_reward(player)
+        win_rate = self.win_rate(player)
+        total_win_rate = self.total_win_rate()
+        fold_rate = self.fold_rate(player)
+        win_as_napoleon_rate = self.win_as_role_rate(player, ROLE_NAPOLEON)
+        win_as_adjutant_rate = self.win_as_role_rate(player, ROLE_ADJUTANT)
+        win_as_ally_rate = self.win_as_role_rate(player, ROLE_ALLY)
+
+        writer.add_scalar("eval/reward", reward, step)
+        writer.add_scalar("eval/win rate", win_rate, step)
+        writer.add_scalar("eval/win rate diff", win_rate - total_win_rate, step)
+        writer.add_scalar("eval/fold rate", fold_rate, step)
+        writer.add_scalar("eval/napoleon/win rate", win_as_napoleon_rate, step)
+        writer.add_scalar("eval/adjutant/win rate", win_as_adjutant_rate, step)
+        writer.add_scalar("eval/ally/win rate", win_as_ally_rate, step)
